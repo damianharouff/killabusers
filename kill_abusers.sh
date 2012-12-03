f859cec3410227a8f9d81ae0d25250f0f1196a92
damian <damian@carbon.ipxcore.com>
2012-12-03 09:26:20 -0700

    minor updates

--- kill_abusers.sh
+++ kill_abusers.sh
@@ -4,7 +4,6 @@ PROCS='dos2.pl stealth kaiten dos.pl exploit msfconsole ddos tfn-child tfn-daemo
 vadimii vadim2 vadimI xdestroy xshock udp.pl trash trash2 synsend synk synk7 synhose stream stream2 smurf5 smurf6 smack slice2 slice3 sl2
 sl3 rc8 overdrop nestea juno da.sh bloop alpha udp2.pl udp test.pl slowaris';
 for i in $PROCS; do
-#if [[ $(pgrep -l $i) ]]; then
 if [[ $(ps axu|grep -i "$i"|grep -v grep|awk '{ print $2 }') ]]; then
 for F in $(ps axu|grep -i "$i"|grep -v grep|awk '{ print $2 }'); do
 
@@ -13,6 +12,5 @@ done
 
 echo "`date ` Abuser found issuing: # kill -9 $i" >> /var/log/abusers.log
 fi
-#echo $i;
 done
 

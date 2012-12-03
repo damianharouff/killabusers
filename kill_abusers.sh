c9eca234c82cb92734e130187d3816cb9402f1bd
damian <damian@carbon.ipxcore.com>
2012-12-03 09:25:05 -0700

    added slowaris

--- kill_abusers.sh
+++ kill_abusers.sh
@@ -2,7 +2,7 @@
 
 PROCS='dos2.pl stealth kaiten dos.pl exploit msfconsole ddos tfn-child tfn-daemon trinoo lool slap.pl brute pscan2 SpyEyeCollector trinity shaft vadimII
 vadimii vadim2 vadimI xdestroy xshock udp.pl trash trash2 synsend synk synk7 synhose stream stream2 smurf5 smurf6 smack slice2 slice3 sl2
-sl3 rc8 overdrop nestea juno da.sh bloop alpha udp2.pl udp test.pl';
+sl3 rc8 overdrop nestea juno da.sh bloop alpha udp2.pl udp test.pl slowaris';
 for i in $PROCS; do
 #if [[ $(pgrep -l $i) ]]; then
 if [[ $(ps axu|grep -i "$i"|grep -v grep|awk '{ print $2 }') ]]; then
